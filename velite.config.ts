61ecb3f45f1c26d1cc0f6421aaef4d7397f6ca17
smais007 <smaisshawon@gmail.com>
2025-03-02 00:20:31 +0600

    setup shadcn ui and config velite for mdx

---
+++ velite.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig, s, defineCollection } from "velite";
+
+const computedFields = <T extends { slug: string }>(data: T) => ({
+  ...data,
+  slugAsParams: data.slug.split("/").slice(1).join("/"),
+});
+
+const post = defineCollection({
+  name: "Post",
+  pattern: "blog/**/*.mdx",
+  schema: s
+    .object({
+      slug: s.string(),
+      title: s.string(),
+      description: s.string().max(999).optional(),
+      date: s.isodate(),
+      published: s.boolean().default(true),
+      body: s.mdx(),
+    })
+    .transform(computedFields),
+});
+
+export default defineConfig({
+  root: "content",
+  output: {
+    data: ".velite",
+    assets: "public/static",
+    base: "/static/",
+    name: "[name]-[hash:6].[ext]",
+    clean: true,
+  },
+  collections: {
+    post,
+  },
+});
